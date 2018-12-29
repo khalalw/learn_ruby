84a56be3d67372514d941794a969e862d239bb29
Khalal Walker <khalal.walker@gmail.com>
2018-12-29 01:04:26 -0800

    pass first two tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
-#write your code here
+# write your code here
+def translate(str)
+  pig_str = ''
+  word_arr = str.split(' ')
+  word_arr.map do |x|
+    x = if /[aeiou]/.match(x[0])
+          x + 'ay'
+        else
+          x[1...x.length] + x[0] + 'ay'
+        end
+    pig_str += x + ' '
+  end
+  pig_str.strip
+end
