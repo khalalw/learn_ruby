4663482e1748986b93f692122e9ebf7e8c2e091f
Khalal Walker <khalal.walker@gmail.com>
2018-12-28 20:08:21 -0800

    finish calculator and update specs

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
-#write your code here
+# write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(arr)
+  num = 0
+  arr.each do |x|
+    num += x
+  end
+  num
+end
+
+def multiply(*nums)
+  product = 1
+  nums.each do |x|
+    product *= x
+  end
+  product
+end
+
+def power(num, exp)
+  num**exp
+end
+
+def factorial(num)
+  return 1 if (0..1).cover?(num)
+
+  num * factorial(num - 1)
+end
