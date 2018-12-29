dd59b5a3a83ae61d377ff9763ceb3da7c7f69145
Khalal Walker <khalal.walker@gmail.com>
2018-12-29 13:52:52 -0800

    finish book title capitalization solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    title_arr = @title.split(' ')
+
+    exceptions = %w[the a an and in of]
+    always = ['i']
+
+    title_arr.each do |word|
+      word.capitalize! unless exceptions.include? word
+
+      word.capitalize if always.include? word
+    end
+
+    title_arr[0].capitalize!
+    @title = title_arr.join(' ')
+  end
 end
