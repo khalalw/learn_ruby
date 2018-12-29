592c9213ef27e7c7e9e692720219ca0d888219ae
Khalal Walker <khalal.walker@gmail.com>
2018-12-29 14:15:48 -0800

    complete timer solution

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize(seconds = 0)
+    @seconds = seconds
+  end
+
+  def time_string
+    secs = @seconds % 60
+    mins = @seconds / 60
+    hrs = mins / 60
+    mins -= (hrs * 60)
+
+    "#{padded(hrs)}:#{padded(mins)}:#{padded(secs)}"
+  end
+
+  def padded(num)
+    if num.to_s.length < 2
+      '0' + num.to_s
+    else
+      num.to_s
+    end
+  end
 end
