3a8a3b67ac432f714449217c8ff910565c102772
Khalal Walker <khalal.walker@gmail.com>
2018-12-29 12:43:42 -0800

    commit pig latin solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,11 +3,12 @@ def translate(str)
   pig_str = ''
   word_arr = str.split(' ')
   word_arr.map do |x|
-    x = if /[aeiou]/.match(x[0])
-          x + 'ay'
-        else
-          x[1...x.length] + x[0] + 'ay'
-        end
+    if /[aeiou]/.match(x[0])
+      x += 'ay'
+    else
+      match_length = /^[^aeiou]qu|^qu|^[^aeiou]+/.match(x).to_s.length
+      x = x[match_length...x.length] + x[0...match_length] + 'ay'
+    end
     pig_str += x + ' '
   end
   pig_str.strip
